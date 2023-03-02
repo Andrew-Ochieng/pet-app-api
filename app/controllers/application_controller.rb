d63f30eda7a536cecb39b085262d4dc9ba32cf08
Code-X <andrewomosh44@gmail.com>
2023-03-02 16:32:08 +0300

    add models && seed data

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,74 @@
 class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
   
-  # Add your routes here
+  # To enable cross origin requests for all routes:
+  set :bind, '0.0.0.0'
+  configure do
+    enable :cross_origin
+  end
+  before do
+    response.headers['Access-Control-Allow-Origin'] = '*'
+    response.headers['Access-Control-Allow-Headers'] = '*'
+    response.headers["Access-Control-Allow-Methods"] = "GET, POST, OPTIONS, PUT, DELETE"
+  end
+  
+  # routes...
+  options "*" do
+    response.headers["Allow"] = "GET, PUT, POST, DELETE, OPTIONS"
+    response.headers["Access-Control-Allow-Headers"] = "Authorization, Content-Type, Accept, X-User-Email, X-Auth-Token"
+    response.headers["Access-Control-Allow-Origin"] = "*"
+    200
+  end
+
+
+  # GET ---------------------------------------------------------------------------# Add your ro utes here
   get "/" do
     { message: "Good luck with your project!" }.to_json
   end
+
+  get "/pets" do
+    pets = Pet.all
+    pet.to_json
+  end
+
+  get "/pets/:id" do
+    pet = Pet.find(params[:id])
+    pet.to_json(only: [:name, :topic, :details])
+  end
+
+  get "/users" do
+    pet.all.to_json
+  end
+
+  get "user/:id" do
+    pet = Pet.find(params[:id])
+    pet.to_json(only: [:name, :topic, :details])
+  end
+
+
+  # POST ---------------------------------------------------------------------------
+  post "/pets" do
+    pet = Pet.create(pet_params)
+    pet.to_json
+  end
+
+
+
+  # PATCH ---------------------------------------------------------------------------
+  patch "/pets/:id" do
+    pet = Pet.find_by(id: params[:id])
+    pet.update()
+    pet.to_json
+  end
+
+
+
+  # DELETE ---------------------------------------------------------------------------
+  delete "/pets/:id" do
+    pet = pet.find(params[:id])
+    pet.destroy
+    pet.to_json
+  end
+
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,21 @@
-puts "🌱 Seeding spices..."
+puts "🌱 Seeding users ..."
 
-# Seed your database here
+# Make 10 users
+10.times do
+  User.create(
+      username: Faker::Name.name,
+      password: Faker::Internet.password(min_length: 8)
+  )
+end
+
+  # Make 50 pets
+50.times do
+  Pet.create(
+      user_id: user.id,
+      name: Faker::Creature::Cat.name,
+      breed: Faker::Creature::Cat.breed,
+      image_url: Faker::Avatar.image(slug: "my-own-slug", size: "50x50", format: "jpg")
+  )
+end
 
 puts "✅ Done seeding!"
